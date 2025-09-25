0eee37e6b2e50316ff23b842eed06f14ee01c35f
srimannarayana <srimannarayana.yasam@gmail.com>
2025-09-25 16:19:21 -0700

    Day-03 files into repo

---
+++ Day-03/output.tf
@@ -0,0 +1,8 @@
+output "ip" {
+    value = aws_instance.dev.public_ip
+  
+}
+output "az" {
+    value = aws_instance.dev.availability_zone
+  
+}
---
+++ Day-03/variable.tf
@@ -0,0 +1,8 @@
+variable "instance_ami" {
+    type = string
+    default = "" 
+}
+variable "instance_type" {
+    type = string
+    default = ""
+}
